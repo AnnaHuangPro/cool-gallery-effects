52bfc9f8e4c0fbf58cf453188fc2972c4e69ffbf
huangYiYuan <414771197@qq.com>
2018-07-08 16:52:47 +0800

    首页图片的修改

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ function addPhotos(){
                 '<strong>快试一试吧（多加几张美图效果更佳哟）</strong>';
             let _html = template
                 .replace('{{index}}',0)
-                .replace('{{image}}',"/photo/indexPhoto.png")
+                .replace('{{image}}',"https://github.com/AnnaHuangPro/cool-gallery-effects/blob/master/photo/404.png")
                 .replace('{{caption}}',"我的背后可有秘籍哦~")
                 .replace('{{desc}}',desc);
             html.push(_html);
